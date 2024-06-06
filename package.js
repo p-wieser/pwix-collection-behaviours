7f00a3d5665c8fbf87c1cd6429f6c4aa5aa784c0
Pierre Wieser <p.wieser@trychlos.org>
2024-06-06 13:41:10 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:collection-behaviours',
-    version: '2.0.1',
+    version: '2.0.2-rc',
     summary: 'Define and attach behaviours to collections',
     git: 'https://github.com/p-wieser/pwix-collection-behaviours'
 });
